744b9e2fb855ebad880a089cc4d21dbc612e4d44
Arya Segara <aryasegara10@gmail.com>
2024-08-10 17:37:53 +0700

    nambah-ssl-file-di-database

--- backend/api/database.js
+++ backend/api/database.js
@@ -6,11 +6,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 
 // koneksi ke database
